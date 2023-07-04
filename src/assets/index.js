078ddb4e3414f27023ed60969b1eea55fe20a6f9
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-04 12:48:30 -0600

    added assets

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import LinkedIn from "./linkedin.svg";
+import DevTo from "./dev-to.svg";
+import Instagram from "./instagram.svg";
+import Facebook from "./facebook.svg";
+import GitHub from "./github.svg";
+import LeetCode from "./leetcode.svg";
+import GFG from "./GeeksforGeeks.svg";
+import Eye from "./eye.svg";
+import HackerRank from "./hackerrank.svg";
+import Moon from "./moon.svg";
+import Sun from "./sun.svg";
+
+export { LinkedIn, DevTo, Instagram, Facebook, GitHub, LeetCode, GFG, Eye, HackerRank, Moon, Sun };
