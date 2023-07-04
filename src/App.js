666439ee99b64c575888f1bf33d54a5967da00b9
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-04 12:51:19 -0600

    updated + added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import NavBar from "./components/NavBar"
-import Summary from "./components/Summary"
-import Skills from "./components/Skills"
-import Education from "./components/Education"
-import Experiences from "./components/Experiences"
-import Projects from "./components/Projects"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Project from "./pages/Project/Project";
+import Education from "./pages/Education/Education";
+import Experience from "./pages/Experience/Experience";
+import HomePage from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Summary />
-      <Skills />
-      <Education />
-      <Experiences />
-      <Projects />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/projects" element={<Project />} />
+          <Route exact path="/education" element={<Education />} />
+          <Route exact path="/experience" element={<Experience />} />
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
