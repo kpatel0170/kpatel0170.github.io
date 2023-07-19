6e26e1b39f39eb8118d03b10287aaec70dd19299
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-19 15:44:44 -0600

    updated UI for experience page

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const ExperienceBlock = ({ experience }) => {
+  useEffect(() => {
+    // Track page view when the component mounts
+    window.gtag('config', 'G-SD76JRWJJP', {
+      page_path: window.location.pathname,
+    });
+  }, []);
   return (
     <div className="flex flex-col mb-8">
       <div className="flex items-start">
-        <div className="w-4 h-4 bg-blue-500 rounded-full flex-shrink-0 mt-1"></div>
+        <div className="w-4 h-4 bg-blue-500 absolute -left-2 rounded-full z-10 md:mt-0"></div>
         <div className="ml-4">
           <h2 className="text-xl font-bold">{experience.role}</h2>
           <h3 className="text-lg font-medium">{experience.company}</h3>
@@ -38,18 +44,19 @@ const Experience = () => {
   const experienceData = require("./Experience.json");
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Experience</h1>
-      <p className="mb-4">{experienceData.header}</p>
-      <div className="space-y-8">
-        {experienceData.experience.map((experience, index) => (
-          <div key={index} className="relative">
-            <ExperienceBlock experience={experience} />
-            {index !== experienceData.experience.length - 1 && (
-              <div className="timeline-connector absolute bg-blue-500 h-full w-px left-2 top-7"></div>
-            )}
+    <div className="flex flex-col justify-center items-center my-10">
+      <div className="relative flex flex-col items-center max-w-[95%] mx-auto p-3">
+        <div className="container mx-auto py-8">
+          <h1 className="text-3xl font-bold mb-4">Experience</h1>
+          <p className="mb-4">{experienceData.header}</p>
+          <div className="space-y-8 border-l-2 border-blue-400">
+            {experienceData.experience.map((experience, index) => (
+              <div key={index} className="relative">
+                <ExperienceBlock experience={experience} />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
