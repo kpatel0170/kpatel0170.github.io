6e9d6c5eaaa1bb679a982ac2e76470464f40a324
kpatel0170 <kartikpatel0170@gmail.com>
2023-11-30 05:43:59 -0600

    added navSection

---
+++ src/components/NavSection/NavSection.js
@@ -0,0 +1,38 @@
+import React from "react";
+import IconCard from "../IconCard/IconCard";
+import { GraduationCap, Code2, FolderGit2, BookOpenCheck } from "lucide-react";
+
+export const revalidate = 3600;
+
+function NavSection({ content, children }) {
+  const icons = [GraduationCap, Code2, FolderGit2, BookOpenCheck];
+
+  return (
+    <section>
+      <div className="mx-auto flex flex-col items-center gap-8 py-4 px-4 max-w-7xl w-full">
+        <div className="text-center">
+          {content.subheading && (
+            <span className="text-md md:text-md lg:text-lg text-muted-foreground">
+              {content.subheading}
+            </span>
+          )}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {content.cards.map((card, index) => (
+            <a href={card.url} target="_blank" rel="noopener noreferrer">
+              <IconCard
+                key={index}
+                icon={icons[index]}
+                heading={card.heading}
+                description={card.description}
+              />
+            </a>
+          ))}
+        </div>
+        {children}
+      </div>
+    </section>
+  );
+}
+
+export default NavSection;
