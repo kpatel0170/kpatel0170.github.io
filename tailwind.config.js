a5cffa372bf4a6343d4340c74b898dc990d8438f
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-08 13:37:04 -0600

    config: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
     theme: {
       extend: {},
     },
