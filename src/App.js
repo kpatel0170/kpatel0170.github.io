4bd80b29d56188f1ae13be666f3d3417a212d595
kpatel0170 <kartikpatel0170@gmail.com>
2024-02-24 14:45:08 -0600

    Refactor routes and add tests for NavBar and Footer components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Resume from "./components/Resume/Resume";
+import HomePage from "./pages/Home/Home";
 import Project from "./pages/Project/Project";
 import Education from "./pages/Education/Education";
 import Experience from "./pages/Experience/Experience";
-import HomePage from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
-import Resume from "./components/Resume/Resume";
 
 const routes = [
   { path: "/", element: <HomePage /> },
   { path: "/projects", element: <Project /> },
   { path: "/education", element: <Education /> },
   { path: "/experience", element: <Experience /> },
-  { path: "/resume", element: <Resume /> }, 
+  { path: "/resume", element: <Resume /> },
 ];
 
 const Layout = ({ children }) => (
@@ -25,18 +25,16 @@ const Layout = ({ children }) => (
   </div>
 );
 
-function App() {
-  return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {routes.map(({ path, element }, index) => (
-            <Route key={index} path={path} element={<Layout>{element}</Layout>} />
-          ))}
-        </Routes>
-      </Suspense>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {routes.map(({ path, element }, index) => (
+          <Route key={index} path={path} element={<Layout>{element}</Layout>} />
+        ))}
+      </Routes>
+    </Suspense>
+  </Router>
+);
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders NavBar', () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const navBarElement = screen.getByTestId('navbar');
+    expect(navBarElement).toBeInTheDocument();
+  });
+
+  test('renders Footer', () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const footerElement = screen.getByTestId('footer'); 
+    expect(footerElement).toBeInTheDocument();
+  });
 });
