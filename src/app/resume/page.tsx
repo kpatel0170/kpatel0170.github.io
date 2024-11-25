b186019e5b0e89fc24f4f71659aa880b5392b114
kpatel0170 <kartikpatel0170@gmail.com>
2024-11-24 18:24:57 -0600

    feat: create Resume component to display PDF resume in an iframe and remove old redirect implementation

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="h-screen">
+      <iframe src="/resume.pdf" className="w-full h-full" />
+    </div>
+  );
+}
