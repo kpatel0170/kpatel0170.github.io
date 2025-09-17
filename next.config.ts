52bc39b72118e9ea727da05fb06f9427b1711324
kpatel0170 <kartikpatel0170@gmail.com>
2025-09-16 18:37:44 -0600

    fix: update TypeScript references and adjust Next.js configuration for production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: "",
-  output: "export",
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export"
+  }),
+  images: {
+    unoptimized: true
+  },
   productionBrowserSourceMaps: false
 };
 
